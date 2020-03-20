3135dde3381fb6025cbed86e11b40de5811c765c
Vaggelis Yfantis <vaggelisyfantis@gmail.com>
2020-03-21 00:21:47 +0200

    Add Google Analytics

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -73,6 +73,16 @@
         </ul>
     </footer>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110449879-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-110449879-2');
+    </script>
+
     <script src="{{ mix('js/main.js', 'assets/build') }}" async></script>
 
     @stack('scripts')
