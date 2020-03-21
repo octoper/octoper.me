9247d73daede27d01b06ddc8be4aceef276f04c6
Vaggelis Yfantis <vaggelisyfantis@gmail.com>
2020-03-21 21:55:38 +0200

    Responsive dark mode toggle (new)

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -33,14 +33,5 @@
                 @include('_assets.at')
             </a>
         </li>
-        <li class="pl-4 flex justify-center">
-            <button title="Dark Mode" x-data="colorMode()"  @click.prevent="toggle()" x-init="init()"
-                class="nav-menu__item dark:text-gray-100 dark-hover:text-white text-gray-700">
-                <svg fill="none" stroke="currentColor" :class="{ 'hidden': mode == false }"
-                    class="fill-current h-6 h-6" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8"><path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
-                <svg fill="none" stroke="currentColor" :class="{ 'hidden': mode == true }"
-                    class="fill-current h-6 h-6" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8"><path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
-            </button>
-        </li>
     </ul>
 </nav>
--- source/_nav/menu-toggle.blade.php
+++ source/_nav/menu-toggle.blade.php
@@ -1,4 +1,12 @@
-<button class="flex justify-center items-center h-10 px-5 lg:hidden focus:outline-none"
+<button title="Dark Mode Toggle" x-data="colorMode()"  @click.prevent="toggle()" x-init="init()"
+    class="flex justify-center dark:text-gray-100 mr-4 p-4 dark-hover:text-white text-gray-700">
+    <svg fill="none" stroke="currentColor" :class="{ 'hidden': mode == false }"
+    class="fill-current h-6 h-6" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8"><path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
+    <svg fill="none" stroke="currentColor" :class="{ 'hidden': mode == true }"
+    class="fill-current h-6 h-6" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8"><path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+</button>
+
+<button class="flex justify-center items-center h-10 px-5 lg:hidden focus:outline"
     onclick="navMenu.toggle()"
 >
     <svg id="js-nav-menu-show" xmlns="http://www.w3.org/2000/svg"
